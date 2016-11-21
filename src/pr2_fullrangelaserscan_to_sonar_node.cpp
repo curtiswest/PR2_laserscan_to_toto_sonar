484bd99f3f0204b0cca907c6817ec886cb57af84
Curtis West <cpw.996@gmail.com>
2016-11-21 18:09:40 +1000

    Add fullrangelaserscan variant for full coverage of sonar regions 7 & 10
    Can now execute pr2_fullrangelaserscan_to_sonar_node to get full coverage values for 7 & 10 (and and also 9 & 10 as usual). This means full 360 coverage of the robot.

--- src/pr2_fullrangelaserscan_to_sonar_node.cpp
+++ src/pr2_fullrangelaserscan_to_sonar_node.cpp
@@ -49,19 +49,19 @@ public:
 
 
 /*
- * Convert front laser scan (/base_scan) into sonar reasons 0-7 * 10-11.
+ * Convert front laser scan (/base_scan) into sonar reasons 0-6 & 11.
  */
 void frontScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 {
   int n_measurements[TOTO_SONARS];
   for (int i=0; i< TOTO_SONARS; i++)
-	{ // skip values obtained from back sonar
-          if ((8!=i) && (9!=i))
+	{ // skip values obtained from back laser scanner
+          if ((7!=i) && (8!=i) && (9!=i) && (10!=i))
               toto_sonars[i]=255.0;
 	  n_measurements[i]=0;
 	}
 
-  //ROS_INFO("NEW FRONT SCAN: Min angle [%f] | Max angle [%f]", scan_in->angle_min, scan_in->angle_max);
+  ROS_INFO("NEW FRONT SCAN: Min angle [%f] | Max angle [%f]", scan_in->angle_min, scan_in->angle_max);
   
   //bool limit = false;
   double  theLaserAperture= scan_in->angle_min;
@@ -71,12 +71,7 @@ void frontScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
   //Loop through each LaserScan angle_increment and adjust reading based on each
   while (theLaserAperture < scan_in->angle_max)
   {  
-	/* s7 */
-	if(theLaserAperture <= -4*myPi/6.0) {
-		sonar_id=7;
-	}
-	/* s6 */
-	else if ( theLaserAperture <= -myPi/2.0 && theLaserAperture > -4*myPi/6.0)
+	  if ( theLaserAperture <= -myPi/2.0 && theLaserAperture > -4*myPi/6.0)
 		{  sonar_id=6; }
 	/* s5 */
     else if (( -myPi/2.0 <= theLaserAperture ) && ( theLaserAperture <= -myPi/3.0 ) )
@@ -99,11 +94,8 @@ void frontScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 	/* s11 */
     else if ( myPi/2.0 < theLaserAperture && 4*myPi/6.0 >= theLaserAperture )
 		{  sonar_id=11; }
-    else if ( 4*myPi/6.0 < theLaserAperture) {
-    	sonar_id=10;
-    }
 
-	if ((8!=i) && (9!=i)) {
+	if ((7!=sonar_id) && (8!=sonar_id) && (9!=sonar_id) && (10!=sonar_id)) {
 	         toto_sonars[sonar_id] =  scan_in->ranges[i] < toto_sonars[sonar_id] ? scan_in->ranges[i] : toto_sonars[sonar_id];
 	}
 	n_measurements[sonar_id]+=1;
@@ -113,7 +105,7 @@ void frontScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 
   // convert meters to centimeters
   for (int i=0; i< TOTO_SONARS; i++)
-	{ if ((8!=i) && (9!=i))
+	{ if ((7!=i) && (8!=i) && (9!=i) && (10!=i))
             toto_sonars[i]=100.0*toto_sonars[i];
                    //ROS_INFO("s4 count: [%d] value: %f",s4_measureemnts,s4 );
 	}
@@ -121,43 +113,35 @@ void frontScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
   //Cap sonar reading to 255 as per MiPAL sonar reading standard
    uint8_t s0_as_int = toto_sonars[0] >255? 255 : uint8_t(toto_sonars[0]);
    sensors.set_sonar(s0_as_int, Sonar::sZero);
-   //ROS_INFO("s0 value: [%d]",sensors.sonar(Sonar::sZero) );
+   ROS_INFO("s0 value: [%d]",sensors.sonar(Sonar::sZero) );
 
    uint8_t s1_as_int = toto_sonars[1] >255? 255 : uint8_t(toto_sonars[1]);
    sensors.set_sonar(s1_as_int, Sonar::sOne);
-   //ROS_INFO("s1 value: [%d]",sensors.sonar(Sonar::sOne) );
+   ROS_INFO("s1 value: [%d]",sensors.sonar(Sonar::sOne) );
 
    uint8_t s2_as_int = toto_sonars[2] >255? 255 : uint8_t(toto_sonars[2]);
    sensors.set_sonar(s2_as_int, Sonar::sTwo);
-   //ROS_INFO("s2 value: [%d]",sensors.sonar(Sonar::sTwo) );
+   ROS_INFO("s2 value: [%d]",sensors.sonar(Sonar::sTwo) );
 
    uint8_t s3_as_int = toto_sonars[3] >255? 255 : uint8_t(toto_sonars[3]);
    sensors.set_sonar(s3_as_int, Sonar::sThree);
-   //ROS_INFO("s3 value: [%d]",sensors.sonar(Sonar::sThree) );
+   ROS_INFO("s3 value: [%d]",sensors.sonar(Sonar::sThree) );
 
    uint8_t s4_as_int = toto_sonars[4] >255? 255 : uint8_t(toto_sonars[4]);
    sensors.set_sonar(s4_as_int, Sonar::sFour);
-   //ROS_INFO("s4 value: [%d]",sensors.sonar(Sonar::sFour) );
+   ROS_INFO("s4 value: [%d]",sensors.sonar(Sonar::sFour) );
 
    uint8_t s5_as_int = toto_sonars[5] >255? 255 : uint8_t(toto_sonars[5]);
    sensors.set_sonar(s5_as_int, Sonar::sFive);
-   //ROS_INFO("s5 value: [%d]",sensors.sonar(Sonar::sFive) );
+   ROS_INFO("s5 value: [%d]",sensors.sonar(Sonar::sFive) );
 
    uint8_t s6_as_int = toto_sonars[6] >255? 255 : uint8_t(toto_sonars[6]);
    sensors.set_sonar(s6_as_int, Sonar::sSix);
-   //ROS_INFO("s6 value: [%d]",sensors.sonar(Sonar::sSix) );
-
-   uint8_t s7_as_int = toto_sonars[7] >255? 255 : uint8_t(toto_sonars[7]);
-   sensors.set_sonar(s7_as_int, Sonar::sSeven);
-   //ROS_INFO("s7 value: [%d]",sensors.sonar(Sonar::sSeven) );
-
-   uint8_t s10_as_int = toto_sonars[10] >255? 255 : uint8_t(toto_sonars[10]);
-   sensors.set_sonar(s10_as_int, Sonar::sTen);
-   //ROS_INFO("s10 value: [%d]",sensors.sonar(Sonar::sTen) );
+   ROS_INFO("s6 value: [%d]",sensors.sonar(Sonar::sSix) );
 
    uint8_t s11_as_int = toto_sonars[11] >255? 255 : uint8_t(toto_sonars[11]);
    sensors.set_sonar(s11_as_int, Sonar::sEleven);
-   //ROS_INFO("s11 value: [%d]",sensors.sonar(Sonar::sEleven) );
+   ROS_INFO("s11 value: [%d]",sensors.sonar(Sonar::sEleven) );
 
    SENSORSSonarSensors_t wb_handler;               // whiteboard
    wb_handler.post(sensors);  
@@ -165,14 +149,14 @@ void frontScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 
 
 /*
- * Convert rear laser scan (/rear_scan) into sonar reasons 8 & 9 .
+ * Convert rear laser scan (/rear_scan) into sonar regioons 7-10.
  */
 void rearScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 {
   int n_measurements[TOTO_SONARS];
   for (int i=0; i< TOTO_SONARS; i++)
 	{ // skip values obtained from front sonar
-          if ((8==i) || (9==i))
+          if ((7==i) || (8==i) || (9==i) || (10==i))
               toto_sonars[i]=255.0;
 	  n_measurements[i]=0;
 	}
@@ -186,17 +170,21 @@ void rearScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 
   //Loop through each LaserScan angle_increment and adjust reading based on each
   while (theLaserAperture < scan_in->angle_max) {
-	if(theLaserAperture > 0 && theLaserAperture <= myPi/6.0) {
-		sonar_id=8;
-	} else if(theLaserAperture < 0 && theLaserAperture >= -myPi/6.0) {
-		sonar_id=9;
-	}
+	 if(theLaserAperture >= myPi/6.0) {
+		 sonar_id = 7;
+	 } else if (myPi/6.0 >= theLaserAperture && theLaserAperture > 0) {
+		 sonar_id = 8;
+	 } else if (0 >= theLaserAperture && theLaserAperture > -myPi/6.0) {
+		 sonar_id = 9;
+	 } else if (theLaserAperture < -myPi/6.0) {
+		 sonar_id = 10;
+	 }
 
 	//Keep only the shortest distance for this sonar range
-	if ((8==sonar_id) || (9==sonar_id)) {
+	if ((7==sonar_id) || (8==sonar_id) || (9==sonar_id) || (10==sonar_id)) {
+			//ROS_INFO("Sonar #[%d] with value [%f]", sonar_id, scan_in->ranges[i]);
 	         toto_sonars[sonar_id] =  scan_in->ranges[i] < toto_sonars[sonar_id] ? scan_in->ranges[i] : toto_sonars[sonar_id];
 	}
-
 	n_measurements[sonar_id]+=1;
 	i++;
 	theLaserAperture+= scan_in->angle_increment;
@@ -204,12 +192,16 @@ void rearScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 
   // convert meters to centimeters
   for (int i=0; i< TOTO_SONARS; i++)
-	{ if ((8==i) || (9==i))
+	{ if ((7==i) || (8==i) || (9==i) || (10==i))
             toto_sonars[i]=100.0*toto_sonars[i];
                    //ROS_INFO("s4 count: [%d] value: %f",s4_measureemnts,s4 );
 	}
 
   //Cap sonar reading to 255 as per MiPAL sonar reading standard
+   uint8_t s7_as_int = toto_sonars[7] >255? 255 : uint8_t(toto_sonars[7]);
+   sensors.set_sonar(s7_as_int, Sonar::sSeven);
+   ROS_INFO("s7 value: [%d]",sensors.sonar(Sonar::sSeven) );
+
    uint8_t s8_as_int = toto_sonars[8] >255? 255 : uint8_t(toto_sonars[8]);
    sensors.set_sonar(s8_as_int, Sonar::sEight);
    ROS_INFO("s8 value: [%d]",sensors.sonar(Sonar::sEight) );
@@ -217,6 +209,10 @@ void rearScanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
    uint8_t s9_as_int = toto_sonars[9] >255? 255 : uint8_t(toto_sonars[9]);
    sensors.set_sonar(s9_as_int, Sonar::sNine);
    ROS_INFO("s9 value: [%d]",sensors.sonar(Sonar::sNine) );
+
+   uint8_t s10_as_int = toto_sonars[10] >255? 255 : uint8_t(toto_sonars[10]);
+   sensors.set_sonar(s10_as_int, Sonar::sTen);
+   ROS_INFO("s10 value: [%d]",sensors.sonar(Sonar::sTen) );
 
    SENSORSSonarSensors_t wb_handler;               // whiteboard
    wb_handler.post(sensors);
